ce4efbaa9c99b60fe626a7534585b5423552eb27
杨锐晨 <xdd@CCTV-1>
2019-01-10 11:58:37 +0800

    asdasd

---
+++ miao/lodash/youngration-lodash.js
@@ -0,0 +1,25 @@
+const youngration = function() {
+  function chunk(ary, sz=1) {
+    const a = []
+    let t = []
+    for (let i = 0; i < ary.length; i++) {
+      t.push(ary[i])
+      if(!((i + 2) % (sz + 1)) || i === ary.length - 1) {
+        a.push(t)
+        t = []
+      }
+    }
+    return a
+  }
+
+
+
+
+
+
+
+/********************************************************/
+  return {
+    chunk: chunk
+  }
+}()
